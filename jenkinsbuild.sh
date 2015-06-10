aaa5303432ef0ed3f8aba336ed93b8023d1a3a86
tdeheurles <tdeheurles@gmail.com>
2015-06-10 11:58:19 +0200

    added jenkinsbuild.sh

---
+++ jenkinsbuild.sh
@@ -0,0 +1,12 @@
+. release.cfg
+
+projectid=$(curl -s 'http://metadata/computeMetadata/v1/project/project-id' -H 'Metadata-Flavor: Google')
+artifactname=gcr.io/$projectid/$servicename:$servicemajor.$serviceminor.$BUILD_NUMBER
+
+
+./build.sh
+
+
+docker build -t $artifactname ./container/
+
+gcloud preview docker push $artifactname
