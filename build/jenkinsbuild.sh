1366b09b4c47787e5aadbdf6ded0418621fd28df
tdeheurles <tdeheurles@gmail.com>
2015-06-10 12:25:22 +0200

    change chmod and build comments

--- build/jenkinsbuild.sh
+++ build/jenkinsbuild.sh
@@ -1,12 +1,13 @@
+# import names
 . ./build/release.cfg
 
+# grab projectid from google
 projectid=$(curl -s 'http://metadata/computeMetadata/v1/project/project-id' -H 'Metadata-Flavor: Google')
 artifactname=gcr.io/$projectid/$servicename:$servicemajor.$serviceminor.$BUILD_NUMBER
 
-
+# build
 ./build/build.sh
-
-
 docker build -t $artifactname ./container/
 
+# push
 gcloud preview docker push $artifactname
